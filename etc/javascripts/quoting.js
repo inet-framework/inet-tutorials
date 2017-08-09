7fc6fe0eb9def86eb2cff708bd7d21641287ff18
Rudolf Hornig <rudi@omnetpp.org>
2017-08-09 14:58:17 +0200

    xml data can be included, too

--- etc/javascripts/quoting.js
+++ etc/javascripts/quoting.js
@@ -7,16 +7,16 @@
 //
 var getLines = function(text, from, until) {
    re = new RegExp("(^.*" + from + "[\\s\\S]*?)\n^.*" + until, 'm'); //TODO alert on regex syntax error!
-   console.log(re);
    matches = text.match(re);
    return matches ? matches[1] : "<b>No matching lines!</b>";
 }
 
 var fileLoaded = function(file, data) {
+   console.log(data);
    pres = $('pre[src="' + file + '"]');
    $.each(pres, function(i,pre) {
       excerpt = getLines(data, pre.attributes.from.value, pre.attributes.until.value);
-      $(pre).html(excerpt);
+      $(pre).text(excerpt);
    });
 };
 
@@ -24,12 +24,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
    pres = $('pre[src]');
    files = new Set();
    $.each(pres, function(i,pre) {files.add(pre.attributes.src.value); });
-   console.log(files);
 
    files.forEach(function(file) {
-      jQuery.get(file, function(data) {
-          fileLoaded(file, data);
-      });
+      jQuery.ajax({url: file,
+                   success: function(data) { fileLoaded(file, data); },
+                   dataType: "text" });
    });
 });
 
