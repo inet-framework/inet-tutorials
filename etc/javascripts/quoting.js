cb1a4fb8ab18acfc7e0387dfccbfbdfccc80f842
Gyorgy Szaszko <adam.szaszko@gmail.com>
2017-08-15 16:10:19 +0200

    added line numbers for includes

--- etc/javascripts/quoting.js
+++ etc/javascripts/quoting.js
@@ -1,32 +1,56 @@
 //
 // Allows including a line range of a file from HTML. Example:
+//
 // <pre src="../WirelessA.ned" from="network WirelessA" until="####"></pre>
+//
 // "from", "until" are regexes that match substring of a line; "from" is inclusive, "until" is exclusive
 //
+// Returns an object/dictionary/map with two values:
+//   startLine - the line number where the returned snippet begins in the whole text (starting from 1 as usual...)
+//   snippet   - the actual matched snippet text
 var getLines = function(text, from, until) {
+    let startLine = 1;
     if (from == null && until == null) {
-        return text;
+        return { startLine: 1, snippet: text };
     } else if (from == null) {
-        re = new RegExp("(^[\\s\\S]*?)\n^.*" + until, 'm'); //TODO alert on regex syntax error!
+        re = new RegExp("()(^[\\s\\S]*?)\n^.*" + until, 'm'); //TODO alert on regex syntax error!
     } else if (until == null) {
-        re = new RegExp("(^.*" + from + "[\\s\\S]*)", 'm'); //TODO alert on regex syntax error!
+        re = new RegExp("([\\s\\S]*?)(^.*" + from + "[\\s\\S]*)", 'm'); //TODO alert on regex syntax error!
     } else {
-        re = new RegExp("(^.*" + from + "[\\s\\S]*?)\n^.*" + until, 'm'); //TODO alert on regex syntax error!
+        re = new RegExp("([\\s\\S]*?)(^.*" + from + "[\\s\\S]*?)\n^.*" + until, 'm'); //TODO alert on regex syntax error!
     }
     matches = text.match(re);
-    return matches ? matches[1] : "!!! No matching lines !!!";
+
+    // Group 1 is the "preface" - everything before the snippet.
+    // Group 2 is the snippet itself.
+    let snippet = matches ? matches[2] : "!!! No matching lines !!!";
+    let preface = matches ? matches[1] : "";
+    startLine = preface.split('\n').length;
+
+    return { startLine: startLine, snippet: snippet };
 }
 
 var fileLoaded = function(file, data) {
    pres = $('pre[src="' + file + '"]');
    $.each(pres, function(i,pre) {
-      excerpt = getLines(data, 
-                         pre.attributes.from ? pre.attributes.from.value : null, 
+      excerpt = getLines(data,
+                         pre.attributes.from ? pre.attributes.from.value : null,
                          pre.attributes.until ? pre.attributes.until.value : null);
-      var language = file.includes(".ned") ? "ned" : file.includes(".xml") ? "xml" : file.includes(".ini") ? "ini" : "generic";
-      Rainbow.color(excerpt, language, function(highlightedCode) {
-            $(pre).html(highlightedCode);
-      });
+
+      var language = file.includes(".ned") ? "ned" : file.includes(".xml") ? "xml"
+          : file.includes(".ini") ? "ini" : file.includes(".py") ? "python" : "generic";
+
+      // this new element will hold some attributes to aid Rainbow and Rainbow.linenumbers, as well as the snippet content.
+      var codeElement = document.createElement('code');
+      codeElement.setAttribute('display', 'none'); // so it won't appear unformatted
+      codeElement.setAttribute('data-language', language); // used by Rainbow
+      codeElement.setAttribute('data-line', excerpt.startLine); // used by Rainbow.linenumbers
+      $(codeElement).text(excerpt.snippet); // making sure we escape it
+      pre.appendChild(codeElement); // adding the <code> to the DOM
+
+      // And then invoke the highlighter on the containing <pre>.
+      // The Rainbow.linenumbers plugin will replace the <code> with a <table>.
+      Rainbow.color(pre);
    });
 };
 
@@ -41,4 +65,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
                    dataType: "text" });
    });
 });
-
